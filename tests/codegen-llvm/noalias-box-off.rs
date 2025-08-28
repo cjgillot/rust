afec69be18fd4e94bb887698b4cc4cb8ff4fb0b1
Camille Gillot <gillot.camille@gmail.com>
2025-08-28 01:47:01 +0000

    Support borrowed locals in DestinationPropagation.

--- tests/codegen-llvm/noalias-box-off.rs
+++ tests/codegen-llvm/noalias-box-off.rs
@@ -2,9 +2,8 @@
 
 #![crate_type = "lib"]
 
-// CHECK-LABEL: @box_should_not_have_noalias_if_disabled(
+// CHECK-LABEL: @box_should_not_have_noalias_if_disabled(ptr {{.*}} %0)
 // CHECK-NOT: noalias
-// CHECK-SAME: %foo)
 #[no_mangle]
 pub fn box_should_not_have_noalias_if_disabled(foo: Box<u8>) {
     drop(foo);
